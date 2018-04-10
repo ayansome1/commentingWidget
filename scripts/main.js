870ab6e9e5ef2412ab61692a9c1a377abaf56426
ayan <ayansome1@gmail.com>
2018-04-10 16:55:40 +0530

    ui update

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,31 @@
-let inputMainComment = document.getElementById("main-comment");
-let submitMainComment = document.getElementById("submit-main-comment");
-let userNameMainComment = document.getElementById("main-commenter-name");
+// let inputMainComment = document.getElementById('main-comment');
+let submitMainComment = document.getElementById('submit-main-comment');
+let userNameMainComment = document.getElementById('main-commenter-name');
 let divCount = 1;
+// let userName;
 
-let addNewDiv = (parentId, data) => {
-    var referenceNode = document.getElementById(parentId);
-    var newNode = document.createElement('div');
-    newNode.id = 'comment-' + divCount;
-    divCount++;
-    newNode.innerHTML = `            <div class="col-sm-12 margin-bottom-30">
+let addNewDiv = (parentId, user, time, comment, like) => {
+	var referenceNode = document.getElementById(parentId);
+	var newNode = document.createElement('div');
+	newNode.id = 'comment-' + divCount;
+	divCount++;
+	newNode.innerHTML = `            
+			<div class="col-sm-12 margin-bottom-30">
                     <div class="col-sm-1">
                         <img src="images/boy.svg" class="user-photo">
                     </div>
                     <div class="col-sm-11">
                         <span class="user-name">
-                        Ayan
+                        ${user}
                     </span>
                         <span class="time-ago">
-                      5 sec ago
+                      ${time}
                     </span>
                         <div class="col-sm-12 padding-0">
-                            daildfnioas fiosa foisa fio asiof isoa foisa fioas fio asiof saio fisaiof iosa fiosa fisa fio saiof saiof iosa fiosa fioas fiosa fio saio fioasio
+                            ${comment}
                         </div>
                         <div class="col-sm-12 padding-0 details margin-top-10">
-                            <span>               4                     </span>
+                            <span>               ${like}                     </span>
                             <img src="images/up-arrow.svg" class="vote margin-right-10">
                             <img src="images/down-arrow.svg" class="vote margin-right-20">
                             <span class="margin-right-20">
@@ -36,16 +38,31 @@ let addNewDiv = (parentId, data) => {
                     </div>
                 </div>`;
 
-    referenceNode.after(newNode);
+	referenceNode.after(newNode);
+};
 
-}
+let submitNewComment = userName => {
+	let comment = document.getElementById('main-comment').value;
 
-let submitNewComment = () => {
+	let user = window.localStorage.getItem('userName');
 
-    let comment = inputMainComment.value;
-    let user = userNameMainComment.value
-    console.log(comment, user);
-    addNewDiv("comments-section", )
-    // let commentsSectionNode = document.getElementById("comments-section");
-    // objTo.appendChild(divtest);
-}
+	// window.localStorage.setItem('userName', userName);
+
+	// let commentsSectionNode = document.getElementById("comments-section");
+	// objTo.appendChild(divtest);
+	let data = window.localStorage.getItem('data');
+	if(!data){
+		data = [];
+	}
+	let time = new Date();
+	let obj = { id: data.length + 1, user: user, comment: comment, time: time, like: 0 };
+	data.push(obj);
+
+	console.log(comment, user);
+	addNewDiv('comments-section', user, time, comment, 0);
+};
+
+let setUserName = () => {
+	userName = document.getElementById('user-name').value;
+	window.localStorage.setItem('userName', userName);
+};
